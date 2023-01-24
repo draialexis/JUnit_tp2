a749276bbf567d4419594d011efbab18ef8fed85
Alexis DRAI <alexis.drai@etu.uca.fr>
2023-01-24 18:00:05 +0100

    :sparkles: Makes PGCD tests pass

--- src/fr/iut/uca/PGCD.java
+++ src/fr/iut/uca/PGCD.java
@@ -2,6 +2,23 @@ package fr.iut.uca;
 
 public class PGCD {
 	public int pgcd(int a, int b) {
-		return 0;
+		if (a < 0) a = -a;
+		if (b < 0) b = -b;
+		
+		if (a < b) {
+			int tmp = a;
+			a = b;
+			b = tmp;
+		}
+		
+		if (b == 0) return a;
+
+		int q = a % b;
+		if(q == 0) {
+			return b;
+		} else {
+			return pgcd(b, q);
+		}
+		
 	}
 }
